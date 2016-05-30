7e1cea94437e2d9ee6ec78c0658a4da2295dc17a
ltaimre <liisitaimre@gmail.com>
2016-05-31 02:00:26 +0300

    lisab elemente, muudab järjekorda

--- Projektihaldur2/script.js
+++ Projektihaldur2/script.js
@@ -1,51 +1,66 @@
 
 $(document).ready(function(){
 	console.log("Loen skripti!");
-	$("#salvestusnupp").click(function(){
-		var tasklist = new Array();
-		$.each($('.actionlist'), function (index, value) { 
-			tasklist[index] = $(value).text();
-			console.log(tasklist[index]); 
-			});	
 	
-});
 
 
 $(".send").submit(function(event) {
   window.location.href="?page=project"
 });
 
-
-$("#change").click(function(){
-		console.log("klikk");
-		document.getElementsByClassName("actionlist").contentEditable = true;
-		document.getElementsByClassName("actionlist").className = "newlist";
-		//var tasklist=document.getElementsByClassName("actionlist");
+  $(function() {
+    $( "#sortable" ).sortable();
+  });
 		
-		//$.each($('.actionlist'), function (index, value) { 
-			//tasklist[index].contentEditable = true;
-			//tasklist[index].className = "newlist";
-//});	
-		
-});
-/*
-	var $elements = $('label');
-$('.cb').change(function() {
-    var $checkbox = $(this);
-    if ($checkbox.prop('checked')) {
-        makeEven($elements);
-    } else {
-        undoEven($elements);
-} */
-
-		
-			
-		
-});
 	
 		
+		
+});
 
+function additem(){
+	//loob listielemendi
+	var node=document.createElement("LI");
+	node.className="taskelement undone";
+	//lisab sellele tekstiväärtuse
+	var newtask = document.getElementById('newtask').value;
+	var textnode=document.createTextNode(newtask);
+	node.appendChild(textnode);
+	//loob checkboxi ja lisab selle listielemendile
+	var input = document.createElement("INPUT");
+	input.setAttribute("type", "checkbox"); 
+	input.className="chbx";
+	input.onclick=  function(){done(this)};
+	node.appendChild(input);
+	//lisab selle dokumenti
+	document.getElementById("sortable").appendChild(node);
+};
 
+function done(e){
+	console.log("töötan");
+	if( $(e).parent().hasClass('undone') ) {
+		$(e).parent().removeClass('undone').addClass('done'); }
+	else
+		
+		{
+		if( $(e).parent().hasClass('done') ) {
+		$(e).parent().removeClass('done').addClass('undone'); }
+		}
+}
+
+function savelist(e){
+		console.log("kohal");
+		var x = document.getElementsByClassName(e);
+		var tasklist = new Array();
+		$.each($(x), function (index, value) { 
+			tasklist[index] = $(value).text();
+			console.log(tasklist[index]); 
+			});	
+			
+		$.post( "?page=project", 
+		{ name: "John", time: "2pm" } );
+		
+
+}
 
 		
 		
